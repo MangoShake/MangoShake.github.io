201e88999c1c5a24e4e28ef8e5a158c6e2ccfb19
Savina Anastasia <nastya.savina.nn@gmail.com>
2017-02-14 20:21:09 +0700

    add package.json and gulpfile.js in scss folder

---
+++ full_project_cars/gulpfile.js
@@ -0,0 +1,1 @@
+'use strict';var gulp = require('gulp');var sass = require('gulp-sass');gulp.task('styles', function() {	gulp.src('scss/*.scss')		.pipe(sass().on('error', sass.logError))    .pipe(gulp.dest('./src/styles/'))});//Watch taskgulp.task('default',function() {    	gulp.watch('scss/*.scss',['styles']);});
